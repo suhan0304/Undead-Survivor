562035b6f9d817315220679a9d3c2c0c36dd3053
ZerrY <suhan0304@gmail.com>
2023-11-03 21:28:05 +0900

    Input System
    - Apply Input System
    - Update Player.cs

--- Undead Survivor/Assets/Undead Survivor/Codes/Player.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/Player.cs
@@ -1,27 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
     public Vector2 inputVec;
     public float speed;
+
     Rigidbody2D rigid;
-        
+    SpriteRenderer spriter;
+
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
     }
-
-    void Update()
-    {
-        inputVec.x = Input.GetAxisRaw("Horizontal");
-        inputVec.y = Input.GetAxisRaw("Vertical");
-    }
-
+    
     void FixedUpdate()
     {
-        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
         rigid.MovePosition(rigid.position + nextVec);
+    }
+
+    void OnMove(InputValue value)
+    {
+        inputVec = value.Get<Vector2>();
+    }
+
+    void LateUpdate()
+    {
+        if (inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
