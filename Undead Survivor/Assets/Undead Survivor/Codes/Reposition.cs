9d81e87d85c35db1e1074ff1328e77d861f5d8a6
ZerrY <suhan0304@gmail.com>
2023-11-04 17:53:41 +0900

    Enemy Movement
    - Implement Enemy Obejct
    - Implementing logic for monsters to track players

--- Undead Survivor/Assets/Undead Survivor/Codes/Reposition.cs
+++ Undead Survivor/Assets/Undead Survivor/Codes/Reposition.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 public class Reposition : MonoBehaviour
 {
+    Collider2D coll;
+
+    void Awake()
+    {
+        coll = GetComponent<Collider2D>();
+    }
+
     //Trigger가 check된 collider에서 나갔을때 호출
     void OnTriggerExit2D(Collider2D collision)
     {
+
         if(!collision.CompareTag("Area")) //나간 Collision의 Tag가 Area가 아니면 바로 return
             return;
 
@@ -28,25 +36,29 @@ public class Reposition : MonoBehaviour
         switch (transform.tag)
         {
             case "Ground": //collsion의 태그가 ground일 경우
-                if ( Mathf.Abs(diffX - diffY) <= 0.1f) { // 모서리 부분에서 그라운드 이동이 정상적이지 않은 오류를 보완하기 위한 코드
-                    transform.Translate(Vector3.right * dirX * 40);
-                    transform.Translate(Vector3.up * dirY * 40);
-                }
-                else if (diffX > diffY) //두 오브젝트의 거리 차이에서 X축이 Y축보다 크면 맵을 수평이동
+                if (diffX > diffY) //두 오브젝트의 거리 차이에서 X축이 Y축보다 크면 맵을 수평이동
                 {
                     //Translate 지정된 값 만큼 현재 위치에서 이동
                     transform.Translate(Vector3.right * dirX * 40);  //오른쪽 단위 벡터(1, 0, 0) * 방향(왼쪽 -1 , 오른쪽 1) * 크기(40) 
-                                                                //크기가 40인 이유는 타일맵을 4개를 사용해서 2*2로 설정했기 때문
+                                                                     //크기가 40인 이유는 타일맵을 4개를 사용해서 2*2로 설정했기 때문
                 }
                 else if (diffX < diffY)  //두 오브젝트의 거리 차이에서 Y축이 X축보다 크면 맵을 수평이동
                 {
                     //Translate 지정된 값 만큼 현재 위치에서 이동
                     transform.Translate(Vector3.up * dirY * 40); 
                 }
+                else
+                {
+                    transform.Translate(dirX * 40, dirY * 40, 0);
+                }
                 break;
 
             case "Enemy": //collsion의 태그가 Enemy일 경우
-
+                if (coll.enabled) // 콜라이더가 활성화 되어있는지 조건 먼저 작성 - 몬스터가 죽었으면 비활성화 되어있음 => 재배치 필요가 없음
+                {
+                    transform.Translate(playerDir * 20 + new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f), 0f)); 
+                    //적어도 맵 크기만큼은 이동 -> 플레이어의 시야 밖에서 재배치 되도록 
+                }
                 break;
         }
 
